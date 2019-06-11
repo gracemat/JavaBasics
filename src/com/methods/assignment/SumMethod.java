61a39c3a02387fd76d833e1819c18a8e67ec7145
realgreeshma <greeshmahere@gmail.com>
2019-06-11 14:45:51 -0500

    String Functions

---
+++ src/com/methods/assignment/SumMethod.java
@@ -0,0 +1,7 @@
+package com.methods.assignment;
+
+public class SumMethod {
+	
+
+
+}
---
+++ src/com/stringmanipulations/StringFunctions.java
@@ -0,0 +1,47 @@
+package com.stringmanipulations;
+
+public class StringFunctions {
+	public static void main(String[] args) {
+		
+		String str = "Hey I am automation king";
+		
+		System.out.println(str.length());//24
+		
+		System.out.println(str.charAt(4));//I
+		
+		System.out.println(str.charAt(23));//g
+		
+		//System.out.println(str.charAt(24));// IndexOutOfBoundsException
+		
+		System.out.println(str.indexOf('a'));
+		
+		int indofa = str.indexOf('a');
+		
+		System.out.println(str.indexOf('a', str.indexOf('a')+1));
+		
+		System.out.println(indofa);
+		
+		int secindofa = str.indexOf('a', str.indexOf('a')+1);
+		System.out.print("Second 'a' Index is:  ");	
+		System.out.println(str.indexOf('a', secindofa+1));
+		
+		
+		String mesg = "Welcome Yashaswin user";
+		
+		System.out.println(mesg.substring(8,mesg.indexOf('u')));
+		
+		String msg = "Welcome Pradyumna user";
+		
+		System.out.println(msg.substring(8,msg.indexOf('u')));//it worked if there is no 'u' in admin name
+		
+		System.out.println(msg.substring(8,msg.length()));
+		
+		System.out.println(msg.substring(8,msg.length()-5));
+		
+		String st = " Selenium Sessions   Started !!!  ";
+		System.out.println(st.trim());
+		System.out.println(st.replace(" ",""));
+		
+	}
+
+}
